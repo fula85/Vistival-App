9fba448d23380507715773fd2554d0db16fa49e8
mobapp06 <mobapp06@dt-c205-14.local>
2019-01-31 15:31:50 +0100

    Initial Commit

---
+++ Vistival App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vistival App
+//
+//  Created by mobapp06 on 31/01/2019.
+//  Copyright © 2019 EhB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
